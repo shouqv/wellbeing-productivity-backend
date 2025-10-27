57317e67d0f8e48349b23ee31184ac7c41d84412
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-10-27 12:58:26 +0300

    adding CRUD (get , post , delete and put) for the model goal and have tested it in postman

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import GoalsIndex , GoalDetail
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-
+path('goals/', GoalsIndex.as_view(), name='goal_index'),
+path('goals/<int:goal_id>/', GoalDetail.as_view(), name='goal_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,79 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated ,IsAuthenticatedOrReadOnly
+from .serializers import GoalSerializer , TaskSerializer , EmotionSerializer
 
 from django.shortcuts import get_object_or_404
-
+from .models import Goal , Task , Emotion
 # Create your views here.
+
+class GoalsIndex(APIView):
+    # TODO - for now the permission is allow any to streamline testing apis
+    permission_classes = [AllowAny]
+    def get(self , request):
+        try:
+            # TODO - dont forget to uncomment the below once auth is done
+            # queryset = Goal.objects.filter(user = request.user)
+            queryset = Goal.objects.all()
+            serializer = GoalSerializer(queryset , many = True)
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error': str(error)} , status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    
+    # REMEMBER: the below is the post req needed
+    # {
+    #     "content": "to let the test succedd",
+    #     "status": "active",
+    #     "year": 2025,
+    #     "user": 1
+    # }
+
+    def post(self, request):
+        try:
+            serializer = GoalSerializer(data=request.data)
+            # TODO - check if the user id is automatically checked if it exist from the is_valid
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+            
+            
+class GoalDetail(APIView):
+    # TODO - for now the permission is allow any to streamline testing apis
+    permission_classes = [AllowAny]
+    def put(self, request, goal_id):
+        try:
+            queryset = get_object_or_404(Goal, id=goal_id)
+            serializer = GoalSerializer(queryset, data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            ) 
+            
+    def delete(self, request, goal_id):
+        try:
+            queryset = get_object_or_404(Goal, id=goal_id)
+            queryset = get_object_or_404(Goal, id=goal_id)
+            user_id = queryset.user.id
+            queryset.delete()
+            return Response(
+                {"message": f"Goal {goal_id} has been deleted for user {user_id}"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
